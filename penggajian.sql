7be03100ecfbb14ffe8c2e1061952ce38aea4c12
Galang Hanafi <galanghanafi8@gmail.com>
2022-06-15 05:01:00 +0700

    Add SQL

--- penggajian.sql
+++ penggajian.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 14, 2022 at 11:47 PM
+-- Generation Time: Jun 14, 2022 at 11:53 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
